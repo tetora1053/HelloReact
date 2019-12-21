648072d9e0ebc5f56d398c5a8a77ccb39ad4e32e
kurikuto <k.ab.kurikuto@gmail.com>
2019-12-21 18:09:40 +0900

    time/buttonコンポーネント作成

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,14 +1,70 @@
 import React from 'react';
 
 class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {time: Date()};
+    this.setTime = this.setTime.bind(this);
+  }
+
+  setTime(date){
+    this.setState((state, props) => {
+      return {time: date};
+    });
+  }
+
   render()
   {
     return (
     <div>
       <h1>Hello World</h1>
+      <Button setTime={this.setTime}/>
+      <Time time={this.state.time}/>
     </div>
     );
   }
+
+  
 }
+
+class Button extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.getTime = this.getTime.bind(this);
+  }
+
+  getTime(){
+    console.log(Date());
+    this.props.setTime(Date());
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="button" onClick={this.getTime}>
+          getDate
+        </button>
+      </div>
+    );
+  }
+}
+
+class Time extends React.Component {
+  
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      <p>本日は{this.props.time}です。</p>
+      </div>
+    );
+  }
+}
+
 
 export default App;

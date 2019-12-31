347b53a7d80e8d0e866028e3dcfd0510902c1920
shinoda <tetora1053@gmail.com>
2019-12-31 16:51:58 +0900

    getDateボタンを押した回数を表示する機能追加

--- helloworld/src/Button.js
+++ helloworld/src/Button.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import Count from './Count'
 
 class Button extends React.Component {
 
   constructor(props){
     super(props);
+    this.state = {count: 0};
     this.getTime = this.getTime.bind(this);
+    this.incrementCount = this.incrementCount.bind(this);
   }
 
   getTime(){
     console.log(Date());
     this.props.setTime(Date());
+
+    this.incrementCount();
+  }
+
+  incrementCount() {
+    this.setState({
+      count: this.state.count + 1
+    });
   }
 
   render() {
@@ -18,6 +29,7 @@ class Button extends React.Component {
         <button className="button" onClick={this.getTime}>
           getDate
         </button>
+        <Count count={this.state.count}/>
       </div>
     );
   }
